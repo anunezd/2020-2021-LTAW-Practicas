43c6da301dee085db8136ecac5e9da65c9a05109
anunezd <alejandro_nd93@hotmail.com>
2021-05-13 11:06:21 +0200

    EJ3 OK

---
+++ P4/index.js
@@ -0,0 +1,23 @@
+var ip = require("ip");
+console.log("Hola desde el proceso de la web...");
+
+//-- Obtener elementos de la interfaz
+const btn_test = document.getElementById("btn_test");
+const display = document.getElementById("display");
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
+
+//-- Acceder a la API de node para obtener la info
+//-- Sólo es posible si nos han dado permisos desde
+//-- el proceso princpal
+info1.textContent = process.versions.node;
+info2.textContent = process.versions.chrome;
+info3.textContent = process.versions.electron;
+info4.textContent = ip.address();
+
+btn_test.onclick = () => {
+    display.innerHTML += "TEST! ";
+    console.log("Botón apretado!");
+}
---
+++ P4/main.js
@@ -0,0 +1,150 @@
+//-- Cargar el módulo de electron
+const electron = require('electron');
+//-- Cargar las dependencias
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const colors = require('colors');
+const PUERTO = 8080;
+
+//-- Crear las variables
+var users = 0;
+var names = {};
+var days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+              'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log("Arrancando electron...");
+
+//-- Variable para acceder a la ventana principal
+//-- Se pone aquí para que sea global al módulo principal
+let win = null;
+
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
+electron.app.on('ready', () => {
+
+    //-- Crear la ventana principal de nuestra aplicación
+    win = new electron.BrowserWindow({
+        width: 600,   //-- Anchura 
+        height: 600,  //-- Altura
+
+        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
+    });
+
+  //-- En la parte superior se nos ha creado el menu
+  //-- por defecto
+  //-- Si lo queremos quitar, hay que añadir esta línea
+  //win.setMenuBarVisibility(false)
+
+  //-- Cargar contenido web en la ventana
+  //-- La ventana es en realidad.... ¡un navegador!
+  //win.loadURL('https://www.urjc.es/etsit');
+
+  //-- Cargar interfaz gráfica en HTML
+  win.loadFile("index.html");
+
+});
+
+var ip = require("ip");
+console.log( ip.address() );
+
+//-- Imprimir por terminal              
+http.listen(PUERTO, () => {
+    console.log('Entra al chat en: http://localhost:' + PUERTO + '/')
+});
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Definir el punto de entrada principal de mi chat web
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+});
+
+//-- Esto es necesario para que el servidor le envíe al cliente la
+//-- biblioteca socket.io para el cliente
+app.use('/', express.static(__dirname +'/'));
+
+//------------------- GESTION SOCKETS IO
+//-- Evento: Nueva conexion recibida
+io.on('connection', (socket) => {
+    
+    //-- Evento hola
+    socket.on('hello', (msg) => {
+      if (isAccepted(msg)) {
+        console.log(msg.red + ' se ha unido al chat'.green);
+        io.emit('msg','<strong>server</strong>: ' + msg + ' se ha unido al chat');
+        users += 1;
+        names[socket.id] = msg;
+        socket.emit('welcome', '<strong>server</strong>: bienvenido, ' + msg);
+      } else {
+        console.log('usuario ' + msg.red + ' no aceptado, el nick ya esta en uso'.green);
+        socket.emit('used', 'Usuario ' + msg + ' no aceptado, el nick ya esta en uso');
+      }
+    })
+
+    //-- Evento de mensaje recibido: Reenviarlo a todos los clientes conectados
+    socket.on('msg', (msg) => {
+      console.log(names[socket.id].red + ": " + msg.blue);
+      //-- Reenviarlo a todos los clientes conectados
+      io.emit('msg', '<strong>' + names[socket.id] + '</strong>: ' + msg);
+    });
+
+    // Eventos de la cmd
+    socket.on('cmd', (msg) => {
+      console.log(names[socket.id].red + ': ' + msg.blue)
+      let cmd = ''
+      switch (msg) {
+        case '/help':
+          cmd += 'puedes usar estos comandos:'
+          cmd += '<ul><li>\'/list\': número de usuarios conectados</li>'
+          cmd += '<li>\'/hello\': saludos desde el servidor</li>'
+          cmd += '<li>\'/date\': mostrar fecha</li>'
+          cmd += '<li>\'/usr-online\': mostrar usuarios conectados</li></ul>'
+          break
+        case '/list':
+          cmd += users.toString() + ' usarios conectados'
+          break
+        case '/hello':
+          cmd = 'Valar Morghulis, ' + names[socket.id]
+          break
+        case '/date':
+          let date = new Date()
+          cmd += days[date.getDay()] + ' ' + date.getDate() + ' de ' + months[date.getMonth()] + ' de ' + date.getFullYear()
+          break;
+        case '/usr-online':
+          cmd += '<ul>'
+          for (let id in names) {
+            cmd += '<li>' + names[id] + '</li>'
+          }
+          cmd += '</ul>'
+          break
+        default:
+          cmd = 'comando\'' + msg + '\' no detectado'
+      }
+      socket.emit('msg', '<strong>server</strong>: ' + cmd)
+    });
+
+    //-- Evento de desconexión
+    socket.on('disconnect', function(){
+      console.log(names[socket.id].red + ' ha dejado el chat'.green);
+      users -= 1;
+      io.emit('msg', '<strong>server</strong>: ' + names[socket.id] + ' ha dejado el chat');
+      delete names[socket.id];
+    });
+
+});
+
+function isAccepted(nick) {
+  let accepted = true;
+  for (let id in names) {
+    if (nick.toLowerCase() == names[id].toLowerCase()) {
+      accepted = false;
+    }
+  }
+  return accepted;
+}
